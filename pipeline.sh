4626ec8dd471f760cf8e2cd993c612161c6a1bef
Igor Gruntov <gruntov99@mail.ru>
2025-08-24 21:36:54 +0300

    v45 test flow

---
+++ pipeline.sh
@@ -0,0 +1,6 @@
+cd /home/git/kufar
+git pull origin master
+kill -9 $(ps aux | grep 'kufar.jar' | awk '{print $2}') 2>/dev/null
+./gradlew build
+./gradlew copyJar
+nohup java -jar kufar.jar > kufarV.log 2>&1 &
