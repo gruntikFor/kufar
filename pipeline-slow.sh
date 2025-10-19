501e6fb79bcb583ad91f93076216ed95d791a7ed
Igor Gruntov <gruntov99@mail.ru>
2025-10-19 21:49:40 +0300

    v59 accelerated build

---
+++ pipeline-slow.sh
@@ -0,0 +1,9 @@
+#sudo systemctl start mongod
+cd /home/git/kufar
+git pull origin master
+kill -9 $(ps aux | grep 'kufar.jar' | awk '{print $2}') 2>/dev/null
+chmod +x ./gradlew
+./gradlew --stop
+./gradlew build
+./gradlew copyJar
+nohup java -jar kufar.jar > kufarV.log 2>&1 &
